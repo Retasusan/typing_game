c52d7ddc0ebf880937a3639dfdd4da1f6c762536
Kenta Goto <kenta9736@gmail.com>
2024-08-18 07:24:42 +0900

    initialize

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="text-center">
+        <h1>Typing game</h1>
+        <p>test description.</p>
+      </div>
+    </main>
+  );
+}
