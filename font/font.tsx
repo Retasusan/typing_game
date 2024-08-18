551561af8be1739dc457cc7b3b21f83dd8aa778d
Kenta Goto <kenta9736@gmail.com>
2024-08-18 20:02:58 +0900

    入力した単語と等しければ次の単語にうつり、正誤数も表示できるようにした

---
+++ font/font.tsx
@@ -0,0 +1,7 @@
+import { Lora } from "next/font/google";
+
+export const font = Lora({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
--- src/app/TypingGame.tsx
+++ src/app/TypingGame.tsx
@@ -3,40 +3,61 @@ import Data from "../../data.json";
 import { useState, useEffect } from "react";
 
 export default function TypingGame() {
-  const numb = Math.floor(Math.random() * Data.length);
+  const numb = 0;
   const [data, setData] = useState(Data[numb]);
-  const handleEnterkey = (event: KeyboardEvent) => {
-    if (event.key === "Enter") {
-      const rand = Math.floor(Math.random() * Data.length);
-      console.log(rand);
-      setData(Data[rand]);
-    }
-  };
-  useEffect(() => {
-    document.addEventListener("keydown", handleEnterkey);
-    return () => {
-      document.removeEventListener("keydown", handleEnterkey);
-    };
-  }, [data.word]);
   const [inputWord, setInputWord] = useState("");
-
+  const [previousNumber, setPreviousNumber] = useState(0);
+  const [rand, setRand] = useState(Math.floor(Math.random() * Data.length));
+  const [correct, setCorrect] = useState(0);
+  const [incorrect, setIncorrect] = useState(0);
   useEffect(() => {
+    let count = 0;
+
     const handleKeyPress = (event: KeyboardEvent) => {
-      setInputWord((prev) => prev + event.key);
+      if (event.key === data.word[count]) {
+        count++;
+        setCorrect((prev) => prev + 1);
+        setInputWord((prev) => prev + event.key);
+        if (count === data.word.length) {
+          setInputWord("");
+          count = 0;
+          setRand(Math.floor(Math.random() * Data.length));
+          if (rand !== previousNumber) {
+            setData(Data[rand]);
+            setPreviousNumber(rand);
+          } else {
+            console.log(rand);
+            let temp = Math.floor(Math.random() * Data.length);
+            while (temp === previousNumber) {
+              temp = Math.floor(Math.random() * Data.length);
+            }
+            setData(Data[temp]);
+            console.log(temp);
+            setPreviousNumber(temp);
+          }
+        }
+      } else {
+        setIncorrect((prev) => prev + 1);
+      }
     };
-
     window.addEventListener("keydown", handleKeyPress);
-
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
-  }, []);
+  }, [data.word]);
   return (
-    <div className="w-4/5 h-[600px] mx-auto justify-center items-center text-center bg-green-500">
-      <h1 className="text-5xl">test word</h1>
-      <p className="text-3xl">number:{data.id}</p>
-      <p className="text-3xl">word:{data.word}</p>
-      <p>{inputWord}</p>
+    <div className="w-4/5 h-[600px] pt-[50px] mx-auto justify-center items-center text-centerborder-none bg-gradient-to-b from-slate-500 to-indigo-500 max-w-[1000px]">
+      <div className="w-4/5 h-[250px] mx-auto bg-blue-300 pt-[80px]">
+        {/* <p className="text-3xl">number:{data.id}</p> */}
+        <p className="text-6xl text-center mt-[15px]">{data.word}</p>
+      </div>
+      <div className="w-4/5 mt-[40px] h-[200px] mx-auto border-none bg-gradient-to-b from-slate-400 to-slate-100">
+        <p className=" pt-[65px] text-6xl text-center">{inputWord}</p>
+      </div>
+      <div className="ml-14">
+        <p>correct{correct}</p>
+        <p>wrong{incorrect}</p>
+      </div>
     </div>
   );
 }
