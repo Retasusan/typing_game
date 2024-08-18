6b8da64e0e2e7eaa110850bfd0a47f287b97a184
Kenta Goto <kenta9736@gmail.com>
2024-08-18 20:29:24 +0900

    細かい修正

--- src/app/TypingGame.tsx
+++ src/app/TypingGame.tsx
@@ -3,16 +3,17 @@ import Data from "../../data.json";
 import { useState, useEffect } from "react";
 
 export default function TypingGame() {
-  const numb = 0;
+  const numb = Math.floor(Math.random() * Data.length);
   const [data, setData] = useState(Data[numb]);
   const [inputWord, setInputWord] = useState("");
   const [previousNumber, setPreviousNumber] = useState(0);
   const [rand, setRand] = useState(Math.floor(Math.random() * Data.length));
   const [correct, setCorrect] = useState(0);
   const [incorrect, setIncorrect] = useState(0);
+  const [finish_word, setFinishWord] = useState(0);
+
   useEffect(() => {
     let count = 0;
-
     const handleKeyPress = (event: KeyboardEvent) => {
       if (event.key === data.word[count]) {
         count++;
@@ -25,6 +26,7 @@ export default function TypingGame() {
           if (rand !== previousNumber) {
             setData(Data[rand]);
             setPreviousNumber(rand);
+            setFinishWord((prev) => prev + 1);
           } else {
             console.log(rand);
             let temp = Math.floor(Math.random() * Data.length);
@@ -34,6 +36,7 @@ export default function TypingGame() {
             setData(Data[temp]);
             console.log(temp);
             setPreviousNumber(temp);
+            setFinishWord((prev) => prev + 1);
           }
         }
       } else {
@@ -46,17 +49,20 @@ export default function TypingGame() {
     };
   }, [data.word]);
   return (
-    <div className="w-4/5 h-[600px] pt-[50px] mx-auto justify-center items-center text-centerborder-none bg-gradient-to-b from-slate-500 to-indigo-500 max-w-[1000px]">
-      <div className="w-4/5 h-[250px] mx-auto bg-blue-300 pt-[80px]">
-        {/* <p className="text-3xl">number:{data.id}</p> */}
-        <p className="text-6xl text-center mt-[15px]">{data.word}</p>
+    <div>
+      <div className="w-4/5 h-[600px] pt-[50px] mx-auto justify-center items-center text-centerborder-none bg-gradient-to-b from-slate-500 to-indigo-500 max-w-[1000px] min-w-[600px]">
+        <div className="w-4/5 h-[250px] mx-auto bg-gradient-to-tr from-sky-400 to-orange-400 pt-[80px]">
+          {/* <p className="text-3xl">number:{data.id}</p> */}
+          <p className="text-6xl text-center mt-[15px]">{data.word}</p>
+        </div>
+        <div className="w-4/5 mt-[40px] h-[200px] mx-auto border-none bg-gradient-to-b from-slate-400 to-slate-100">
+          <p className=" pt-[65px] text-6xl text-center">{inputWord}</p>
+        </div>
       </div>
-      <div className="w-4/5 mt-[40px] h-[200px] mx-auto border-none bg-gradient-to-b from-slate-400 to-slate-100">
-        <p className=" pt-[65px] text-6xl text-center">{inputWord}</p>
-      </div>
-      <div className="ml-14">
-        <p>correct{correct}</p>
-        <p>wrong{incorrect}</p>
+      <div className="ml-[10%]">
+        <p>correct:{correct}</p>
+        <p>wrong:{incorrect}</p>
+        <p>finished words:{finish_word}</p>
       </div>
     </div>
   );
